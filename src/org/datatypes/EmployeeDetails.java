d09ed6992529f0e6ab356a4d46b2ad7043a77c83
VIJATAMILARASAN <vijayengg96@gmail.com>
2025-04-21 12:51:57 +0530

    Java Code Added

---
+++ src/org/datatypes/EmployeeDetails.java
@@ -0,0 +1,39 @@
+package org.datatypes;
+
+public class EmployeeDetails {
+	
+	public static void main(String[] args) {
+		
+		String empId = "AVL009";
+		
+		System.out.println("Employee ID is :"+empId);
+		
+		String empName = "Vijay";
+		
+		System.out.println("Employee Name is :"+empName);
+		
+        String empEmail = "vijayengg96@gmail.com";
+        
+        System.out.println("Employee Email Address is :"+empEmail);
+        
+        long empPhone = 98942196622l;
+        
+        System.out.println("Enployee Phone Number is :"+empPhone);
+        
+        int empSalary = 85000;
+        
+        System.out.println("Employee Salary is :"+empSalary);
+        
+        char empGender = 'M';
+        
+        System.out.println("Employee Gender is :"+empGender);
+        
+        String empCity = "Needamangalam";
+        
+        System.out.println("Employee City is :"+empCity);
+        
+        		
+        	
+	}
+
+}
